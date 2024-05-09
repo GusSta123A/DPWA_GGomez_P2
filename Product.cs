a8df8850b72926989bfc65d26ee7f2e0143ed5e6
GusSta123A <49495787+GusSta123A@users.noreply.github.com>
2024-05-08 18:34:46 -0600

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,9 @@
+﻿namespace DPWA_GGomez_P2
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+    }
+}
---
+++ ProductDB.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DPWA_GGomez_P2
+{
+    public class ProductDB : DbContext
+    {
+        public ProductDB(DbContextOptions<ProductDB> options) : base(options) { }
+        public DbSet<Product> Products => Set<Product>();
+    }
+        
+}
